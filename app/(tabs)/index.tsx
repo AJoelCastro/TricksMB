2c231e58309278b5bec359751b5ffbe12084598c
Castro Reyes Arturo Joel <misthorn33@gmail.com>
2025-01-15 22:17:43 -0500

    tailwind

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,14 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import {View} from 'react-native'
-import {Text} from 'react-native'
+import {View, Text} from 'react-native';
 import "../../global.css";
-import 'nativewind';
+
 
 export default function HomeScreen() {
-  return (
-    <View >
-      <Text className ="text-white">Arturo cabezon</Text>
-    </View>
-  );
+    return (
+        <View className ="text-white">
+            Arturo cabezon
+            <View>Arturo cabezon </View>
+        </View>
+    );
 }
 
 
