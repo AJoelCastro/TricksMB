ac8183924acfb5437c66f15f231f5372ec24a155
Pandaman <147335739+Pandaman08@users.noreply.github.com>
2025-01-17 17:18:58 -0500

    Hedear_2.0

--- app/home.jsx
+++ app/home.jsx
@@ -1,31 +1,30 @@
-import {View, Text, TouchableOpacity} from 'react-native';
-import "../global.css";
-
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { Link, Redirect } from 'expo-router';
+import React from 'react';
+import { SafeAreaView, Text, TouchableOpacity } from 'react-native';
+import { Link } from 'expo-router';
 import CustomButtom from '@/components/customButtom';
 import Header from '@/components/Header';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-export default function index() {
+export default function Index() {
     return (
-        <SafeAreaView className='h-full bg-white flex items-center'>
+        <SafeAreaView className="h-full bg-white flex">
+            {/* Componente Header */}
             <Header
                 title="Almacén seleccionado"
-                //leftIcon=""
-                //rightIcon=""
+                LeftIcon={() => <Icon name ="cog" size = {24} color="black"/>} // Pasas el componente Icon como LeftIcon
+                RightIcon={() => <Icon name="user-circle" size={24} color="black" />} // Ícono derecho
                 onLeftPress={() => console.log("Configuración presionada")}
                 onRightPress={() => console.log("Perfil presionado")}
             />
-            <TouchableOpacity  className=' bg-black h-8 w-32'>
-                <Link href={"/editar"}>
-                    <Text className='text-white'>Editar</Text>
+
+            {/* Contenido principal */}
+            <TouchableOpacity className="bg-black h-8 w-32 mt-5">
+                <Link href="/editar">
+                    <Text className="text-white">Editar</Text>
                 </Link>
             </TouchableOpacity>
-            <CustomButtom
-            title="Ordenes de Produccion"
-            >
 
-            </CustomButtom>
+            <CustomButtom title="Ordenes de Producción" />
         </SafeAreaView>
     );
 }
