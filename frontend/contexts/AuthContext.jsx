8a07b3ae419181967743642848142a0e35b1a530
Castro Reyes Arturo Joel <misthorn33@gmail.com>
2025-02-08 22:32:22 -0500

    token

--- frontend/contexts/AuthContext.jsx
+++ frontend/contexts/AuthContext.jsx
@@ -1,7 +1,7 @@
 // contexts/AuthContext.jsx
 import React, { createContext, useState, useEffect, useContext } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import jwtDecode from 'jwt-decode';
+import {jwtDecode} from 'jwt-decode';
 
 const AuthContext = createContext();
 
@@ -12,31 +12,28 @@ export const AuthProvider = ({ children }) => {
     // Verificar token al cargar la app
     useEffect(() => {
         const loadToken = async () => {
+        //     await AsyncStorage.removeItem('token'); // Elimina el token
+        // console.log("Token eliminado correctamente.");
             const token = await AsyncStorage.getItem('token');
             console.log("Token cargado desde AsyncStorage:", token);
             if (token) {
-                if (isTokenExpired(token)) {
-                    // Eliminar el token expirado
-                    await AsyncStorage.removeItem('token');
-                    setUserToken(null);
-                } else {
-                    setUserToken(token);
+                try {
+                    const decoded = jwtDecode(token);
+                    if (decoded.exp * 1000 > Date.now()) {
+                        console.log(decoded.exp*1000);
+                        console.log(Date.now())
+                        setUserToken(token);
+                    } else {
+                        await logout(); // Token , elimínalo
+                    }
+                } catch (error) {
+                    await logout(); // Token inválido
                 }
             }
-            
             setIsLoading(false);
         };
         loadToken();
     }, []);
-
-    const isTokenExpired = (token) => {
-        try {
-            const decoded = jwtDecode(token);
-            return decoded.exp * 1000 < Date.now();
-        } catch {
-            return true;
-        }
-    };
 
     const login = async (token) => {
         await AsyncStorage.setItem('token', token);
