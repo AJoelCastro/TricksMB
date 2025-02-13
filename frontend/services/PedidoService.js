80c46476678ba4a81ffc9ed5030dfdbeb50126bb
Pandaman <147335739+Pandaman08@users.noreply.github.com>
2025-02-12 23:18:24 -0500

    peticiones

--- frontend/services/PedidoService.js
+++ frontend/services/PedidoService.js
@@ -5,10 +5,19 @@ const API_URL = process.env.EXPO_PUBLIC_API_URL;
 
 const PedidoService = {
     
-    crearPeido : async () =>{
-        
+    crearPeido : async (datosPedido) =>{
+        try{
+            const token = await AuthService.getToken();
+            if(!token) throw new Error("No hay token disponible");
+            
+            const response = await axios.post(`${API_URL}/pedido/crear`,datosPedido,{
+                headers: {Authorization: `Bearer${token}`}
+            });
+
+            return response.data;
+        } catch(error){
+            console.error("Error al crear pedido: ", error.response?.data || error.message);
+            throw error;
+        }
     }
-
-
-
 }
