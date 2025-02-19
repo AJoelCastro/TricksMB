a7a60e1d138eb454c9cbdf2dfd70f18c3cfb6f4c
Pandaman <147335739+Pandaman08@users.noreply.github.com>
2025-02-19 13:33:05 -0500

    rutaTipo

--- backend/routes/TipoCalzadoRoutes.js
+++ backend/routes/TipoCalzadoRoutes.js
@@ -2,6 +2,6 @@ const expres = express();
 const tipoCalzadoController = require('../controllers/TipoCalzadoController')
 const authMiddleware = require('../middlewares/authMiddleware');
 
-expres.get('/todos',authMiddleware,tipoCalzadoController.getAllTipoCalzado)
-
+expres.get('/todos',authMiddleware,tipoCalzadoController.getAllTipoCalzado);
+expres.get('/nombre',authMiddleware,tipoCalzadoController.getTipoCalzadoByNombre);
 module.exports = expres;
