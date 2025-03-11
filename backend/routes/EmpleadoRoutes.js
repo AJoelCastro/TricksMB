b0ca39344e943f687b6ae69bd51bc4eb44e898c0
Pandaman <147335739+Pandaman08@users.noreply.github.com>
2025-03-11 18:49:49 -0500

    si funca

--- backend/routes/EmpleadoRoutes.js
+++ backend/routes/EmpleadoRoutes.js
@@ -1,10 +1,13 @@
 const EmpleadoController = require("../controllers/EmpleadoController");
+const DetalleEmpleadoPedidoController = require("../controllers/DetalleEmpleadoPedidoController");
 
 const express = require('express');
+const authMiddleware = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.post("/crear", EmpleadoController.createEmpleado);
 router.get("/buscar/:dni", EmpleadoController.getByDni);
 router.get("/buscarPorArea", EmpleadoController.getEmpleados);
+router.post("/crearDetalleEmpleadoPedido",authMiddleware, DetalleEmpleadoPedidoController.createDetalleEmpleadoPedido);
 
 module.exports = router;
