5378ab30f933441cdfbf3583afd7f73b4ceb5999
Castro Reyes Arturo Joel <misthorn33@gmail.com>
2025-03-23 12:20:11 -0500

    camera

--- frontend/app/(menu)/inventariado/index.jsx
+++ frontend/app/(menu)/inventariado/index.jsx
@@ -1,12 +1,16 @@
-import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+import { CameraView, useCameraPermissions } from 'expo-camera';
+import { Image } from 'expo-image';
 import { useState } from 'react';
-import { Button, Text, TouchableOpacity, View } from 'react-native';
+import { Button, Pressable, Text, TouchableOpacity, View } from 'react-native';
+import { Switch } from 'react-native-paper';
 
 const Inventario=() =>{
   const [facing, setFacing] = useState('back');
-  console.log("facing");
   const [permission, requestPermission]=useCameraPermissions();
   console.log("permission", permission);
+  const [qrLeido, setQrLeido] = useState(false);
+  const [showCamera, setShowCamera] = useState(false);
+  const [showRegisters, setShowRegisters] = useState(true);
   if (!permission) {
     // Camera permissions are still loading.
     return <View />;
@@ -22,24 +26,57 @@ const Inventario=() =>{
     );
   }
 
-  function toggleCameraFacing() {
+  const toggleCameraFacing = () => {
     setFacing(current => (current === 'back' ? 'front' : 'back'));
   }
 
+  const handreLeerQr = () => {
+    
+  }
   return (
     <View className='flex-1'>
-      <View>
-        <CameraView 
-          facing={facing}
-          onBarcodeScanned={({data}) => console.log(data)}
-        >
-          <View className='h-[70%]'>
-            <TouchableOpacity  onPress={toggleCameraFacing}>
-              <Text>Flip Camera</Text>
-            </TouchableOpacity>
-          </View>
-        </CameraView>
+      <View className='flex-row justify-between p-4'>
+        <Text className='font-normal text-2xl'>Abrir camara</Text>
+        <Switch value={showCamera} onValueChange={setShowCamera} />
       </View>
+      <View>
+        {
+          showCamera&&(
+            <CameraView 
+              facing={facing}
+              onBarcodeScanned={qrLeido ? undefined : handreLeerQr}
+            >
+              <View className='h-[70%]'>
+                <TouchableOpacity  onPress={toggleCameraFacing}>
+                  <Text>Flip Camera</Text>
+                </TouchableOpacity>
+              </View>
+            </CameraView>
+          )
+          
+        }
+      </View>
+      {
+        showRegisters?(
+          <View className='flex-row justify-center gap-4'>
+            <View className="bg-[#634AFF] p-4 rounded-lg">
+              <Pressable onPressOut={()=>setShowRegisters(false)}>
+                <Text className='text-white'>
+                  Registrar Ingreso
+                </Text>
+              </Pressable>
+            </View>
+            <View className="bg-[#634AFF] p-4 rounded-lg">
+              <Pressable>
+                <Text className='text-white'>
+                  Registrar Salida
+                </Text>
+              </Pressable>
+            </View>
+          </View>
+        ):null
+      }
+      
     </View>
   );
 }
