5db92161afadc67f0bebb35936c47e9d58686de3
Pandaman <147335739+Pandaman08@users.noreply.github.com>
2025-03-26 17:55:54 -0500

    id

--- backend/dao/ClienteDAO.js
+++ backend/dao/ClienteDAO.js
@@ -151,12 +151,14 @@ class ClienteDAO {
 
     static async getClientesById() {
         try{
-            const query = `SELECT cn.Dni, cj.Ruc 
+            const query = `SELECT COALESCE(cn.Dni, cj.Ruc) AS identificador
                 FROM Cliente_natural cn 
                 LEFT JOIN Cliente_juridico cj 
                 ON cn.Cliente_idCliente = cj.Cliente_idCliente
+
                 UNION
-                SELECT cn.Dni, cj.Ruc 
+
+                SELECT COALESCE(cn.Dni, cj.Ruc) AS identificador
                 FROM Cliente_juridico cj
                 LEFT JOIN Cliente_natural cn 
                 ON cj.Cliente_idCliente = cn.Cliente_idCliente`;
