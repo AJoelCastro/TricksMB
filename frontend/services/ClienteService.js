ca21bec036d968d4b35c85aba4ceaff09ca3b84d
Castro Reyes Arturo Joel <misthorn33@gmail.com>
2025-03-29 17:18:27 -0500

    cliente

--- frontend/services/ClienteService.js
+++ frontend/services/ClienteService.js
@@ -32,7 +32,7 @@ const ClienteService = {
     obtenerClientes: async () => {
         try {
             const token = await AuthService.getToken();
-            const response = await axios.get(`${API_URL}/cliente/obtnerTodo`, {
+            const response = await axios.get(`${API_URL}/cliente/obtenerTodo`, {
                 headers: { Authorization: `Bearer ${token}` }
             });
             return response.data;
