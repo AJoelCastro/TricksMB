6b8140fa9bddb656bdd70bddd8bb0fef3af61fde
Castro Reyes Arturo Joel <misthorn33@gmail.com>
2025-03-31 23:50:09 -0500

    a

--- backend/services/PdfService.js
+++ backend/services/PdfService.js
@@ -13,58 +13,66 @@ const ImagenService = require("./ImagenService");
 
 const PdfService = {
     async generatePDF(cajas) {
-        const doc = new PDFDocument({ margin: 50 });
-        const buffers = [];
-        
-        doc.on('data', buffers.push.bind(buffers));
-        
-        return new Promise(async (resolve, reject) => {
-            doc.on('end', () => resolve(Buffer.concat(buffers)));
-
-            let x = 50;
-            let y = 50;
-            const colWidth = 120;
-            const rowHeight = 140;
-            const maxHeight = doc.page.height - 50;
-
+        return new Promise((resolve, reject) => {
             try {
-                for (const caja of cajas) {
-                    const base64Data = caja.qrImage.replace(/^data:image\/png;base64,/, '');
-                    const imageBuffer = Buffer.from(base64Data, 'base64');
-
-                    const caracteristica = await CaracteristicasService.getCaracteristicaByIdCaracteristicas(caja.idCaracteristicas);
-                    const detallePedido = await DetallPedidoService.getDetallePedidoByidDetallePedido(caracteristica.Detalle_pedido_idDetalle_pedido);
-                    const modelo = await ModeloService.getModeloById(detallePedido.Modelo_idModelo);
-                    const tipoCalzado = await TipoCalzadoService.getTipoCalzadoByCodigoPedido(modelo.Tipo_calzado_idTipo_calzado);
-
-                    if (y + rowHeight > maxHeight) {
-                        x += colWidth;
-                        y = 50;
-
-                        if (x + colWidth > doc.page.width - 50) {
-                            doc.addPage();
-                            x = 50;
-                            y = 50;
+                const doc = new PDFDocument({ margin: 50 });
+                const buffers = [];
+    
+                doc.on('data', buffers.push.bind(buffers));
+                doc.on('end', () => resolve(Buffer.concat(buffers)));
+    
+                let x = 50;
+                let y = 50;
+                const colWidth = 120;
+                const rowHeight = 140;
+                const maxHeight = doc.page.height - 50;
+    
+                (async () => {
+                    try {
+                        for (const caja of cajas) {
+                            const base64Data = caja.qrImage.replace(/^data:image\/png;base64,/, '');
+                            const imageBuffer = Buffer.from(base64Data, 'base64');
+    
+                            const caracteristica= await CaracteristicasService.getCaracteristicaByIdCaracteristicas(caja.idCaracteristica);
+                            const detallePedido= await DetallPedidoService.getDetallePedidoByidDetallePedido(caracteristica.Detalle_pedido_idDetalle_pedido);
+                            const modelo= await ModeloService.getModeloById(detallePedido.Modelo_idModelo);
+                            const tipoCalzado= await TipoCalzadoService.getTipoCalzadoByCodigoPedido(modelo.Tipo_calzado_idTipo_calzado);
+                            if (y + rowHeight > maxHeight) {
+                                x += colWidth;
+                                y = 50;
+    
+                                if (x + colWidth > doc.page.width - 50) {
+                                    doc.addPage();
+                                    x = 50;
+                                    y = 50;
+                                }
+                            }
+    
+                            doc.image(imageBuffer, x, y + 15, { width: 120, height: 120 });
+                            doc.text(`Código del pedido: ${detallePedido.Codigo_pedido}`, x, y);
+                            doc.text(`Caja ${caja.idCaja}:`, x, y + 15);
+                            doc.text(`Tipo de calzado: ${tipoCalzado.Nombre}`, x, y + 30);
+                            doc.text(`Modelo: ${modelo.Nombre}`, x, y + 45);
+                            doc.text(`Talla: ${caracteristica.Talla}`, x, y + 60);
+                            doc.text(`Color: ${caracteristica.Color}`, x, y + 75);
+    
+                            y += rowHeight;
                         }
+                    } catch (error) {
+                        const errorPdf = new Error("Error al generar el PDF");
+                        errorPdf.status = 500;
+                        reject(errorPdf);
                     }
-
-                    doc.text(`Código del pedido: ${detallePedido.Codigo_pedido}`, x, y);
-                    doc.text(`Caja ${caja.idCaja}:`, x, y + 15);
-                    doc.text(`Tipo de calzado: ${tipoCalzado.Nombre}`, x, y + 30);
-                    doc.text(`Modelo: ${modelo.Nombre}`, x, y + 45);
-                    doc.text(`Talla: ${caracteristica.Talla}`, x, y + 60);
-                    doc.text(`Color: ${caracteristica.Color}`, x, y + 75);
-                    doc.image(imageBuffer, x, y + 15, { width: 120, height: 120 });
-
-                    y += rowHeight;
-                }
-
+                })();
                 doc.end();
             } catch (error) {
-                reject(new Error("Error al generar el PDF"));
+                const errorPdf2 = new Error("Error general en la generación del PDF");
+                errorPdf2.status = 500;
+                reject(errorPdf2);
             }
         });
     },
+    
 
     async sendPDFToTelegram(pdfBuffer, fileName) {
     try {
