12da98e23e8809639609ab1294bc437434f824f0
Castro Reyes Arturo Joel <misthorn33@gmail.com>
2025-04-05 19:42:56 -0500

    tokens

--- frontend/services/ModeloService.js
+++ frontend/services/ModeloService.js
@@ -7,6 +7,7 @@ const ModeloService = {
     getAllModelo: async () => {
         try {
             const token = await AuthService.getToken();
+            if(!token) throw new Error("⚠️ No se recibió un token en la respuesta del servidor.");
             const response = await axios.get(`${API_URL}/modelo/todos`, {
                 headers: { Authorization: `Bearer ${token}` }
             });
@@ -28,6 +29,7 @@ const ModeloService = {
     getAllModeloById: async (id) => {
         try {
             const token = await AuthService.getToken();
+            if(!token) throw new Error("⚠️ No se recibió un token en la respuesta del servidor.");
             const response = await axios.get(`${API_URL}/modelo/id`, {
                 headers: { Authorization: `Bearer ${token}` },
                 params:{id}
@@ -51,6 +53,7 @@ const ModeloService = {
     getModeloByCodigoPedido: async (codigoPedido) => {
         try {
             const token = await AuthService.getToken();
+            if(!token) throw new Error("⚠️ No se recibió un token en la respuesta del servidor.");
             const response = await axios.get(`${API_URL}/modelo/obtener/${codigoPedido}`, {
                 headers: { Authorization: `Bearer ${token}` }
             });
@@ -72,6 +75,7 @@ const ModeloService = {
     getImagenById: async (idModelo) => {
         try {
             const token = await AuthService.getToken();
+            if(!token) throw new Error("⚠️ No se recibió un token en la respuesta del servidor.");
             const response = await axios.get(`${API_URL}/modelo/imagen/${idModelo}`, {
                 headers: { Authorization: `Bearer ${token}` }
             });
@@ -90,6 +94,28 @@ const ModeloService = {
             }
         }
     },
+    getInventario: async () => {
+        try {
+            const token = await AuthService.getToken();
+            if(!token) throw new Error("⚠️ No se recibió un token en la respuesta del servidor.");
+            const response = await axios.get(`${API_URL}/modelo/stock`, {
+                headers: { Authorization: `Bearer ${token}` }
+            });
+            return response.data;
+        } catch (error) {
+            if (error.response) {
+                // El servidor respondió con un código de error
+                throw new Error(error.response.data.error || `Error en la creación de la caja: ${error.response.status}`);
+            } else if (error.request) {
+                console.log("error.request",error.request);
+                // No hubo respuesta del servidor
+                throw new Error("No se recibió respuesta del servidor. Verifique su conexión.");
+            } else {
+                // Otro tipo de error
+                throw new Error("Ocurrió un error inesperado al crear la caja.");
+            }
+        }
+    }
 };
 
 export default ModeloService;
