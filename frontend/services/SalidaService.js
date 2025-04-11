ff40a231998335f1741725075548d97830943bf3
Castro Reyes Arturo Joel <misthorn33@gmail.com>
2025-04-11 10:29:17 -0500

    ss 2.0

--- frontend/services/SalidaService.js
+++ frontend/services/SalidaService.js
@@ -4,67 +4,39 @@ import AuthService from './AuthService';
 const API_URL = process.env.EXPO_PUBLIC_API_URL;
 
 const SalidaService = {
-  createIngreso: async (idCaja, codigoPedido) => {
-    const token = await AuthService.getToken();
-    if (!token) {
-      throw new Error('No hay token disponible');
-    }
-    try {
-      const response = await axios.post(
-        `${API_URL}/ingreso/crear`,
-        { idCaja, codigoPedido },
-        {
-          headers: { Authorization: `Bearer ${token}` },
+    createSalida: async (idCaja, codigoPedido) => {
+        const token = await AuthService.getToken();
+        if (!token) {
+        throw new Error('No hay token disponible');
         }
-      );
-      return response.data;
-    } catch (error) {
-      if (error.response) {
-        // El servidor respondió con un código de error
-        throw new Error(
-          error.response.data.error ||
-            `Error en la creación de la caja: ${error.response.status}`
+        try {
+        const response = await axios.post(
+            `${API_URL}/salida/crear`,
+            { idCaja, codigoPedido },
+            {
+            headers: { Authorization: `Bearer ${token}` },
+            }
         );
-      } else if (error.request) {
-        console.log('error.request', error.request);
-        // No hubo respuesta del servidor
-        throw new Error(
-          'No se recibió respuesta del servidor. Verifique su conexión.'
-        );
-      } else {
-        // Otro tipo de error
-        throw new Error('Ocurrió un error inesperado al crear la caja.');
-      }
+        return response.data;
+        } catch (error) {
+        if (error.response) {
+            // El servidor respondió con un código de error
+            throw new Error(
+            error.response.data.error ||
+                `Error en la creación de un detalle Salida Service ${error.response.status}`
+            );
+        } else if (error.request) {
+            console.log('error.request', error.request);
+            // No hubo respuesta del servidor
+            throw new Error(
+            'No se recibió respuesta del servidor. Verifique su conexión.'
+            );
+        } else {
+            // Otro tipo de error
+            throw new Error('Ocurrió un error inesperado al crear un detalle salida.');
+        }
+        }
     }
-  },
-  obtenerIngreso: async idCaja => {
-    try {
-      const token = await AuthService.getToken();
-      if (!token) throw new Error('No hay token disponible');
-
-      const response = await axios.get(`${API_URL}/ingreso/obtener/${idCaja}`, {
-        headers: { Authorization: `Bearer ${token}` },
-      });
-      return response.data;
-    } catch (error) {
-      if (error.response) {
-        // El servidor respondió con un código de error
-        throw new Error(
-          error.response.data.error ||
-            `Error en la creación de la caja: ${error.response.status}`
-        );
-      } else if (error.request) {
-        console.log('error.request', error.request);
-        // No hubo respuesta del servidor
-        throw new Error(
-          'No se recibió respuesta del servidor. Verifique su conexión.'
-        );
-      } else {
-        // Otro tipo de error
-        throw new Error('Ocurrió un error inesperado al crear la caja.');
-      }
-    }
-  },
 };
 
 export default SalidaService;
