fc4c58f8e099982f739fc8b3d3a2d0d14b28b891
Castro Reyes Arturo Joel <misthorn33@gmail.com>
2025-04-12 21:47:50 -0500

    salidaController

--- backend/routes/SalidaRoutes.js
+++ backend/routes/SalidaRoutes.js
@@ -5,5 +5,5 @@ const router = express.Router();
 const authMiddleware = require('../middlewares/authMiddleware');
 
 router.post('/crear/:idCaja', SalidaController.createSalida);
-
+router.get("/obtener/:idCaja", SalidaController.getSalidasByCaja);
 module.exports = router;
