1cb740771c9e8c3f6eacd7ccbe61eb9910324f10
Castro Reyes Arturo Joel <misthorn33@gmail.com>
2025-04-24 15:02:34 -0500

    frontend separation

--- backend/routes/AreaTrabajoRoutes.js
+++
@@ -1,7 +0,0 @@
-const AreaTrabajoController = require('../controllers/AreaTrabajoController');
-const express = require('express');
-const router = express.Router();
-
-router.post("/crear", AreaTrabajoController.createAreaTrabajo);
-
-module.exports = router;
