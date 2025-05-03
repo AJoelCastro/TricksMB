b12249aa4181e21b19e207c2aeba39fabc1306cf
ACastroAnalytia <acastro@analytia.pe>
2025-05-02 22:35:07 -0500

    ThemedView

---
+++ components/Dashboard.tsx
@@ -0,0 +1,112 @@
+import React, { useRef, useState, useContext } from 'react';
+import { View, Text, Modal, Pressable, TouchableWithoutFeedback, findNodeHandle, UIManager, SafeAreaView, ScrollView } from 'react-native';
+import { useNavigation, DrawerActions } from '@react-navigation/native';
+import { ThemedView } from '@/components/ThemedView';
+import { ThemedText } from '@/components/ThemedText';
+import TopBar from '@/components/TopBar';
+import { AuthContext } from '@/context/AuthContext';
+import { useThemeColor } from '@/hooks/useThemeColor';
+import { Colors } from '@/constants/Colors';
+import { Divider, Icon } from 'react-native-paper';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const Dashboard = ({ children }: Props) => {
+  const authContext = useContext(AuthContext);
+  const [showActions, setShowActions] = useState(false);
+  const [buttonPosition, setButtonPosition] = useState({ x: 0, y: 0, width: 0, height: 0 });
+  const settingsButtonRef = useRef<View>(null);
+  const name = authContext.user?.name;
+  const email = authContext.user?.email;
+  const navigation = useNavigation();
+
+  const backgroundColor = useThemeColor({ light: Colors.light.background, dark: Colors.dark.background }, 'background');
+  const textColor = useThemeColor({ light: Colors.light.text, dark: Colors.dark.text }, 'text');
+  const iconColor = useThemeColor({ light: Colors.light.icon, dark: Colors.dark.icon }, 'icon');
+  const backIconColor = useThemeColor({ light: Colors.light.backIcon, dark: Colors.dark.backIcon }, 'backIcon');
+  const contentColor = useThemeColor({ light: Colors.light.content, dark: Colors.dark.content }, 'content');
+
+  const openActions = () => {
+    requestAnimationFrame(() => {
+      if (settingsButtonRef.current?.measureInWindow) {
+        settingsButtonRef.current.measureInWindow((x, y, width, height) => {
+          setButtonPosition({ x, y, width, height });
+          setShowActions(true);
+        });
+      }
+    });
+  };
+
+  return (
+    <ThemedView className='h-full' style={{ backgroundColor }}>
+      <TopBar
+        icon1="menu"
+        onPress1={() => navigation.dispatch(DrawerActions.openDrawer())}
+        icon2="bell"
+        icon3="cog"
+        onPress3={openActions}
+        pressed={showActions}
+        settingsButtonRef={settingsButtonRef}
+      />
+      {/* seccion en donde debe de ir el children */}
+      <SafeAreaView>
+        <ScrollView style={{marginBottom:32}} showsVerticalScrollIndicator={false}>{children}</ScrollView>
+      </SafeAreaView>
+
+      {/* Modal de acciones */}
+      <Modal
+        visible={showActions}
+        transparent
+        animationType="fade"
+        onRequestClose={() => setShowActions(false)}
+      >
+        <TouchableWithoutFeedback onPress={() => setShowActions(false)}>
+          <View style={{ flex: 1, backgroundColor: 'transparent' }}>
+            <TouchableWithoutFeedback>
+              <View
+                style={{
+                  top: buttonPosition.y + buttonPosition.height + 10,
+                  right: 5,
+                  position: 'absolute',
+                  backgroundColor: backIconColor,
+                  paddingVertical: 20,
+                  paddingHorizontal: 25,
+                  width: '70%',
+                  height: '25%',
+                  borderRadius: 8,
+                  borderColor: textColor,
+                  borderWidth: 1,
+                  shadowColor: '#000',
+                  shadowOffset: { width: 0, height: 2 },
+                  shadowOpacity: 0.3,
+                  shadowRadius: 5,
+                  elevation: 5,
+                }}
+              >
+                <View className='flex-col gap-3 mb-4'>
+                  <Text style={{ color: textColor, fontSize: 19, fontWeight: 'bold' }}>
+                    {name?.toUpperCase()}
+                  </Text>
+                  <Text style={{ color: textColor, fontSize: 14 }}>{email?.toUpperCase()}</Text>
+                </View>
+                <Divider style={{ backgroundColor: textColor, height: 0.3 }} className='mt-3 mb-4' />
+                <Pressable onPress={() => setShowActions(false)} className='flex-row items-center mt-4 gap-4'>
+                  <Icon source="cog" size={20} color={iconColor} />
+                  <Text style={{ color: textColor, fontSize: 16 }}>Configuración</Text>
+                </Pressable>
+                <Pressable onPress={() => authContext.logOut()} className='flex-row items-center mt-6 gap-4'>
+                  <Icon source="logout" size={20} color={iconColor} />
+                  <Text style={{ color: textColor, fontSize: 16 }}>Cerrar sesión</Text>
+                </Pressable>
+              </View>
+            </TouchableWithoutFeedback>
+          </View>
+        </TouchableWithoutFeedback>
+      </Modal>
+    </ThemedView>
+  );
+};
+
+export default Dashboard;
---
+++ components/TopBar.tsx
@@ -0,0 +1,89 @@
+import * as React from 'react';
+import { Appbar, Avatar } from 'react-native-paper';
+import { ThemedView } from './ThemedView';
+import { useColorScheme, View } from 'react-native';
+import { useThemeColor } from '@/hooks/useThemeColor';
+import { Colors } from '@/constants/Colors';
+type Props = {
+    title?: string;
+    icon1?: string;
+    onPress1?: () => void;
+    icon2?: string;
+    onPress2?: () => void;
+    icon3?: string;
+    onPress3?: () => void;
+    pressed?: boolean;
+    settingsButtonRef?: React.RefObject<any>; // Cambia el tipo según lo que necesites
+};
+const TopBar = ({title, icon1, onPress1, icon2, onPress2, icon3, onPress3, pressed, settingsButtonRef }:Props) => {
+    const backgroundColor = useThemeColor(
+        { light: Colors.light.background, dark: Colors.dark.background },
+        'background'
+    );
+    const textColor = useThemeColor(
+        { light: Colors.light.text, dark: Colors.dark.text },
+        'text'
+    );
+    const iconColor = useThemeColor(
+        { light: Colors.light.icon, dark: Colors.dark.icon },
+        'icon'
+    );
+    const tabColor = useThemeColor(
+        { light: Colors.light.tabIconSelected, dark: Colors.dark.tabIconSelected },
+        'tabIconSelected'
+    );
+    const backIconColor = useThemeColor(
+        { light: Colors.light.backIcon, dark: Colors.dark.backIcon },
+        'backIcon'
+    );
+    const contentColor = useThemeColor(
+        { light: Colors.light.content, dark: Colors.dark.content },
+        'content'
+    );
+    return(
+        <Appbar.Header style={{backgroundColor: backgroundColor, borderBottomWidth: 1, borderBottomColor: tabColor}}>
+            <View
+                style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    backgroundColor: backIconColor, 
+                    borderRadius: 20, 
+                    paddingHorizontal: 2,
+                    paddingVertical: 1,}}
+            >
+                <Appbar.Action icon={icon1!} onPress={onPress1} color={iconColor}/>
+            </View>
+            <Appbar.Content title={title} color={iconColor}/>
+            <View
+                style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    backgroundColor: backIconColor,
+                    borderRadius: 20, 
+                    paddingHorizontal: 2,
+                    paddingVertical: 1,}}
+            >
+                <Appbar.Action icon={icon2!} onPress={onPress2} color={iconColor}/>
+            </View>
+            <View 
+                style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    backgroundColor: pressed ? textColor : backIconColor, 
+                    borderRadius: 20, 
+                    paddingHorizontal: 2,
+                    paddingVertical: 1,
+                    marginLeft: 10
+                    
+                }}
+                collapsable={false}
+                ref={settingsButtonRef}
+            >
+                <Avatar.Text size={40} label='J' style={{ marginLeft: 10 }} />
+                <Appbar.Action icon={icon3!} onPress={onPress3} color={iconColor}/>
+            </View>
+        </Appbar.Header>
+    )
+};
+
+export default TopBar;
