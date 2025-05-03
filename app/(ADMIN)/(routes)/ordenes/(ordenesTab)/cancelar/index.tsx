6709700ac63e862fb23c3a50cc75649750d92d72
ACastroAnalytia <acastro@analytia.pe>
2025-05-03 12:17:50 -0500

    routeo 2.0 issue #288

---
+++ app/(ADMIN)/(routes)/ordenes/(ordenesTab)/cancelar/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Dashboard from '@/components/Dashboard'
+
+const IndexCancelarAdmin = () => {
+  return (
+    <Dashboard>
+        <></>
+    </Dashboard>
+  )
+}
+
+export default IndexCancelarAdmin
---
+++ app/(ADMIN)/(routes)/ordenes/(ordenesTab)/crear/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Dashboard from '@/components/Dashboard'
+
+const IndexCrearAdmin = () => {
+  return (
+    <Dashboard>
+        <></>
+    </Dashboard>
+  )
+}
+
+export default IndexCrearAdmin
