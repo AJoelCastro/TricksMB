de3193d8df1933406d4c1a4b2cec320db043b1ab
AJoelCastro <misthorn33@gmail.com>
2025-05-03 15:56:10 -0500

    delete paginas

--- app/(ADMIN)/(routes)/historial/index.tsx
+++ app/(ADMIN)/(routes)/historial/index.tsx
@@ -1,11 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Dashboard from '@/components/Dashboard'
+import HistorialPedidosAdmin from '@/components/historial/HistorialPedidos'
 
 const IndexHistorialAdmin = () => {
   return (
     <Dashboard>
-        <></>
+        <HistorialPedidosAdmin></HistorialPedidosAdmin>
     </Dashboard>
   )
 }
