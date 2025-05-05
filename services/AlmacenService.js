c4ff8f012e58fba7077922d925ede3a0130be9d8
AJoelCastro <misthorn33@gmail.com>
2025-05-05 00:27:53 -0500

    almacen service

--- services/AlmacenService.js
+++ services/AlmacenService.js
@@ -4,9 +4,17 @@ import AuthService from './AuthService';
 const API_URL = process.env.EXPO_PUBLIC_API_URL;
 
 const AlmacenService = {
-    crearAlmacen: async (idTipoAlmacen) => {
+    crearAlmacen: async (nombre, imagen, direccion) => {
         try {
-            
+            const token = await AuthService.getToken();
+            if (!token) {
+                throw new Error('No hay token disponible');
+            }
+            const response = await axios.post(`${API_URL}/almacen/crear`,{nombre, imagen,direccion},{
+                headers: { Authorization: `Bearer ${token}` },
+                }
+            )
+            return response.data;
         }catch (error) {
             throw error;
         }
