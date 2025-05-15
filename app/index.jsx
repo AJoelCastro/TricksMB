44b60c764fb37508805f5b8e1036ea98280d9c51
AJoelCastro <misthorn33@gmail.com>
2025-05-15 11:31:08 -0500

    cambios dashboard issue #292 7.0

--- app/index.jsx
+++ app/index.jsx
@@ -13,13 +13,14 @@ import { TextInput } from 'react-native-paper';
 import * as SplashScreen from 'expo-splash-screen';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import AuthService from '@/services/AuthService'; // Importar servicio de autenticación
+
 import ShowError from '@/components/ShowError';
 import { AuthContext } from '@/contexts/AuthContext';
-import { useThemeColor } from '@/hooks/useThemeColor';
-import { Colors } from '@/constants/Colors';
-import UserService from '@/services/UserService';
 import { ThemedText } from '@/components/ThemedText';
+import { useAppColors } from '@/hooks/useAppColors';
+
+import AuthService from '@/services/AuthService'; // Importar servicio de autenticación
+import UserService from '@/services/UserService';
 
 SplashScreen.preventAutoHideAsync(); // Asegura que la pantalla de carga no desaparezca antes de tiempo
 const Home = () => {
@@ -28,7 +29,7 @@ const Home = () => {
   const [contrasenia, setContrasenia] = useState(''); // Estado para la contraseña
   const [loading, setLoading] = useState(false); // Estado para indicar si está cargando
   const [showPassword, setShowPassword] = useState(false);
-
+  const { background, text, backIcon, icon } = useAppColors();
   const authContext = useContext(AuthContext);
 
   useEffect(() => {
@@ -61,23 +62,10 @@ const Home = () => {
     }
   }, [authContext.user]);
 
-  const backgroundColor = useThemeColor(
-    { light: Colors.light.background, dark: Colors.dark.background },
-    'background'
-  );
-  const textColor = useThemeColor(
-      { light: Colors.light.text, dark: Colors.dark.text },
-      'text'
-  );
-  const backIconColor = useThemeColor(
-      { light: Colors.light.backIcon, dark: Colors.dark.backIcon },
-      'backIcon'
-  );
-
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       
-      <SafeAreaView className='flex-1 h-full ' style={{position:'relative', backgroundColor: backgroundColor}}>
+      <SafeAreaView className='flex-1 h-full ' style={{position:'relative', backgroundColor: background}}>
         <View  style={{ height: '20%' }} >
           <Image
             source={require('@/assets/images/TricksLogo.png')}
@@ -105,11 +93,11 @@ const Home = () => {
               placeholder='Ingrese su correo'
               mode='outlined'
               value={correo}
-              textColor= {textColor}
-              placeholderTextColor={textColor}
+              textColor= {text}
+              placeholderTextColor={text}
               onChangeText={setCorreo}
               style={{
-                backgroundColor: backIconColor,
+                backgroundColor: backIcon,
               }}
             />
             <TextInput
@@ -117,10 +105,10 @@ const Home = () => {
               placeholder='Ingrese su contraseña'
               mode='outlined'
               style={{
-                backgroundColor: backIconColor,
+                backgroundColor: backIcon,
               }}
-              textColor= {textColor}
-              placeholderTextColor={textColor}
+              textColor= {text}
+              placeholderTextColor={text}
               secureTextEntry={!showPassword}
               value={contrasenia}
               onChangeText={setContrasenia}
@@ -133,8 +121,8 @@ const Home = () => {
             />
           </View>
           <View className='flex-row items-center gap-4 mt-6 ml-16'>
-            <Icon name='check' size={16} color={textColor} />
-            <ThemedText style={{color:textColor}}>Recordar Contraseña</ThemedText>
+            <Icon name='check' size={16} color={icon} />
+            <ThemedText>Recordar Contraseña</ThemedText>
           </View>
 
           {/* Botón de Iniciar Sesión */}
@@ -149,7 +137,7 @@ const Home = () => {
                 disabled={!isFormValid}
                 className={`rounded-md p-4 w-[90%] `}
                 style={{
-                  backgroundColor: backIconColor,
+                  backgroundColor: backIcon,
                   borderWidth: 0.15,
                 }}
               >
--- hooks/useAppColors.ts
+++ hooks/useAppColors.ts
@@ -6,10 +6,12 @@ export const useAppColors = () => {
   const content = useThemeColor({ light: Colors.light.content, dark: Colors.dark.content }, 'content');
   const icon = useThemeColor({ light: Colors.light.icon, dark: Colors.dark.icon }, 'icon');
   const backIcon = useThemeColor({ light: Colors.light.backIcon, dark: Colors.dark.backIcon }, 'backIcon');
-  
+  const background = useThemeColor({ light: Colors.light.background, dark: Colors.dark.background },'background');
+  const text = useThemeColor({ light: Colors.light.text, dark: Colors.dark.text }, 'text');
   return {
     content,
     icon,
     backIcon,
+    background,
   };
 };
