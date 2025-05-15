983aef6f15e162f185900316e48ee0b6c30198dd
AJoelCastro <misthorn33@gmail.com>
2025-05-15 09:11:00 -0500

    cambios dashboard issue #292

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,18 +1,23 @@
-import React, { useRef, useState, useContext } from 'react';
-import { View, Text, Modal, Pressable, TouchableWithoutFeedback, findNodeHandle, UIManager, SafeAreaView, ScrollView } from 'react-native';
+import React, { useRef, useState, useContext, useEffect } from 'react';
+import { View, Text, Modal, Pressable, TouchableWithoutFeedback, Dimensions, ScaledSize, SafeAreaView, ScrollView } from 'react-native';
 import { useNavigation, DrawerActions } from '@react-navigation/native';
-import { ThemedView } from './ThemedView';
+import { ThemedView } from '@/components/ThemedView';
+import { ThemedText } from '@/components/ThemedText';
 import TopBar from '@/components/TopBar';
 import { AuthContext } from '@/contexts/AuthContext';
 import { useThemeColor } from '@/hooks/useThemeColor';
 import { Colors } from '@/constants/Colors';
 import { Divider, Icon } from 'react-native-paper';
 
+import * as ScreenOrientation from 'expo-screen-orientation';
 type Props = {
   children: React.ReactNode;
 };
 
 const Dashboard = ({ children }: Props) => {
+
+  const [height, setHeight] = useState(Dimensions.get('window').height);
+  const [typeScreen, setTypeScreen] = useState<string>('');
   const authContext = useContext(AuthContext);
   const [showActions, setShowActions] = useState(false);
   const [buttonPosition, setButtonPosition] = useState({ x: 0, y: 0, width: 0, height: 0 });
@@ -37,9 +42,58 @@ const Dashboard = ({ children }: Props) => {
       }
     });
   };
+  useEffect(() => {
+    // Se ejecuta cada vez que cambia la orientación o tamaño de la pantala
+    const handleChange = ({ window }: { window: ScaledSize }) => {
+      setHeight(window.height);
+    };
+
+    // Escucha algun cambio de dimensiones
+    const subscription = Dimensions.addEventListener('change', handleChange);
+
+    // Se elimina el listener cuando el componente se desmonta
+    return () => {
+      subscription?.remove?.();
+    };
+  }, []);
+
+  useEffect(() => {
+    // Convertir el valor numérico a un nombre legible
+    const getOrientationName = (orientation: ScreenOrientation.Orientation) => {
+      switch (orientation) {
+        case ScreenOrientation.Orientation.PORTRAIT_UP:
+          return 'Portrait Up';
+        case ScreenOrientation.Orientation.PORTRAIT_DOWN:
+          return 'Portrait Down';
+        case ScreenOrientation.Orientation.LANDSCAPE_LEFT:
+          return 'Landscape Left';
+        case ScreenOrientation.Orientation.LANDSCAPE_RIGHT:
+          return 'Landscape Right';
+        default:
+          return 'Unknown';
+      }
+    };
+
+    // Se ejecuta cada vez que cambia la orientación
+    const subscription = ScreenOrientation.addOrientationChangeListener(event => {
+      const orientation = event.orientationInfo.orientation;
+      setTypeScreen(getOrientationName(orientation));
+    });
+
+    // Obtiene la orientación inicial
+    (async () => {
+      const orientation = await ScreenOrientation.getOrientationAsync();
+      setTypeScreen(getOrientationName(orientation));
+    })();
+
+    // Limpia el listener al desmontar
+    return () => {
+      ScreenOrientation.removeOrientationChangeListener(subscription);
+    };
+  }, []);
 
   return (
-    <ThemedView className='h-full'>
+    <ThemedView className='h-full' style={{ backgroundColor }}>
       <TopBar
         icon1="menu"
         onPress1={() => navigation.dispatch(DrawerActions.openDrawer())}
@@ -73,7 +127,7 @@ const Dashboard = ({ children }: Props) => {
                   paddingVertical: 20,
                   paddingHorizontal: 25,
                   width: '70%',
-                  height: '30%',
+                  height: typeScreen === 'Portrait Up' ? height / 4 : height / 2,
                   borderRadius: 8,
                   borderColor: textColor,
                   borderWidth: 1,
@@ -83,19 +137,20 @@ const Dashboard = ({ children }: Props) => {
                   shadowRadius: 5,
                   elevation: 5,
                 }}
+                className='justify-between'
               >
-                <View className='flex-col gap-3 mb-4'>
+                <View className='flex-col gap-3'>
                   <Text style={{ color: textColor, fontSize: 19, fontWeight: 'bold' }}>
                     {name?.toUpperCase()}
                   </Text>
                   <Text style={{ color: textColor, fontSize: 14 }}>{email?.toUpperCase()}</Text>
                 </View>
-                <Divider style={{ backgroundColor: textColor, height: 0.3 }} className='mt-3 mb-4' />
-                <Pressable onPress={() => setShowActions(false)} className='flex-row items-center mt-4 gap-4'>
+                <Divider style={{ backgroundColor: textColor, height: 0.3 }} />
+                <Pressable onPress={() => setShowActions(false)} className='flex-row items-center gap-4'>
                   <Icon source="cog" size={20} color={iconColor} />
                   <Text style={{ color: textColor, fontSize: 16 }}>Configuración</Text>
                 </Pressable>
-                <Pressable onPress={() => authContext.logOut()} className='flex-row items-center mt-6 gap-4'>
+                <Pressable onPress={() => authContext.logOut()} className='flex-row items-center gap-4'>
                   <Icon source="logout" size={20} color={iconColor} />
                   <Text style={{ color: textColor, fontSize: 16 }}>Cerrar sesión</Text>
                 </Pressable>
